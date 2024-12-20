92705bfd21cab962f21e99e59932a460a2c842a5
朱天文 <zhutianwen@szlanyou.com>
2024-12-20 17:45:39 +0800

    modify route file in 2 folders

--- app/query/route.ts
+++ app/query/route.ts
@@ -14,10 +14,6 @@ async function listInvoices() {
 }
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
   try {
   	return Response.json(await listInvoices());
   } catch (error) {
